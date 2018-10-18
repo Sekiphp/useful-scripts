ef5cbc960a91807a4a347a568ea8f6dfc2c24273
Sekiphp <ucty@diginex.cz>
2018-10-18 08:18:57 +0200

    FIX bug in autoupdate

--- autoupdater.sh
+++ autoupdater.sh
@@ -3,7 +3,7 @@ clear
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 REMOTE_HASH="$( curl -s https://api.github.com/repos/Sekiphp/useful-scripts/commits/master | grep -m1 -o -P '(?<="sha": ")[^"]+' )"
-LOCAL_HASH="$( git rev-parse HEAD)"
+LOCAL_HASH="$( git -C $DIR rev-parse HEAD)"
 
 if [ "$REMOTE_HASH" == "$LOCAL_HASH" ];
 then
