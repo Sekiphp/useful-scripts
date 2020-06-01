32b35459c8c028c54f4a429cf40b00e3b39186ff
Lubos Hubacek <lubos.hubacek@ateli.cz>
2020-06-01 16:54:01 +0200

    Stashing of files only if there are something to stash

--- git/branch_update.sh
+++ git/branch_update.sh
@@ -2,18 +2,29 @@
 clear
 
 CURRENT=$(git name-rev --name-only HEAD)
+CHANGES=$(git status -su | wc -l)
 
+# sync with another branch
 BRANCH="master"
 if [ "$1" = "-b" ] && [ -n "$2" ];
 then
     BRANCH="$2"
 fi
 
-git stash
+# is there something for add to stage?
+if [ "$CHANGES" -ne "0" ];
+then
+	git stash
+fi
+
 git checkout "$BRANCH"
 git pull
 git checkout "$CURRENT"
 git merge "$BRANCH" -m "Update $CURRENT from $BRANCH"
-git stash pop
+
+if [ "$CHANGES" -ne "0" ];
+then
+	git stash pop
+fi
 
 git status
