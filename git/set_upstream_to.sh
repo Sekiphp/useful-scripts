ba0767fa000bf2459ec45cb5e2563df841f52386
Lubos Hubacek <info@luboshubacek.cz>
2025-02-09 19:56:13 +0100

    Eliminate tags references

--- git/set_upstream_to.sh
+++ git/set_upstream_to.sh
@@ -3,7 +3,7 @@
 COLOR_RED="\033[0;31m"
 COLOR_WHITE="\033[0;37m"
 
-CURRENT=$(git name-rev --name-only HEAD)
+CURRENT=$(git symbolic-ref --short HEAD)
 git branch --set-upstream-to=origin/"$CURRENT" "$CURRENT"
 
 echo -e "$COLOR_RED git branch --set-upstream-to=origin/$CURRENT $CURRENT $COLOR_WHITE"
